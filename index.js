0dcb04019f410a8678ace63ed8408efde04b7bae
callixmiller <calmill413@gmail.com>
2023-02-22 15:53:24 -0500

    started function to grab character information, able to grab the characters image on request

--- index.js
+++ index.js
@@ -13,7 +13,25 @@ document.addEventListener('DOMContentLoaded', () => {
             })
         })
     }
-    
+
+    const characterInfo = document.querySelector('.getCharacter')
+    characterInfo.addEventListener('submit', e => {
+        e.preventDefault()
+        
+        const id = document.getElementById('characterNumber').value
+        const characterURL = `https://rickandmortyapi.com/api/character/${id}`
+        fetch(characterURL)
+        .then(res => res.json())
+        .then(data => {
+            const card = Object.values(data)
+            console.log(card)
+            const post = document.getElementById('characterCard')
+            const img = document.createElement('img')
+            img.src = card[8]
+            post.append(img)
+        })
+    })
+
     document.addEventListener('click', e => {
         if(e.target.matches('li')) {
             e.target.style.color = 'red'
